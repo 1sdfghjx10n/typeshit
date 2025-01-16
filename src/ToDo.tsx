4c52221acd5cc4b1f05358bdb26b766daaf83f0d
1sdfghjx10n <jxinyeo5@gmail.com>
2025-01-16 18:44:14 +0800

    final befre repair

--- src/ToDo.tsx
+++ src/ToDo.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 type Priority = 'p1'|'p2'|'p3';
 
 type Task = {
@@ -8,20 +10,17 @@ type Task = {
     };
 
     function ToDo() {
-        const tasks: Task[] = [
-            {
-              id: 1,
-              title: 'Learn React',
-              isCompleted: true,
-              priority: 'p1',
-            },
-          ];
+        const [tasks, setTasks] = React.useState<Task[]>([]);
+        const [taskName, setTaskName] = React.useState('');
 
         return (
             <div>
                 <h1>My Tasks</h1>
                 <label htmlFor="task-input">New Task</label>
-                <input id="task-input" >
+                <input 
+                value={taskName}
+                onChange={(e) => setTaskName(e.target.value)}
+                id="task-input" >
 
                 </input>
                 <ul>
